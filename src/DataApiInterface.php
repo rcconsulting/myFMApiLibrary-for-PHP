60b8343beffc63989ec69f1425ea3f07a2f5a489
JacobTaylor-RCC <39351909+JacobTaylor-RCC@users.noreply.github.com>
2019-09-04 18:15:09 -0700

    updated interface defs to match current code

--- src/DataApiInterface.php
+++ src/DataApiInterface.php
@@ -9,20 +9,27 @@ namespace Lesterius\FileMakerApi;
 interface DataApiInterface
 {
     /**
-     * @param $apiUsername
-     * @param $apiPassword
+     * @param string $apiUsername
+     * @param string $apiPassword
      *
      * @return mixed
      */
-    public function login($apiUsername, $apiPassword);
+    public function login(string $apiUsername, string $apiPassword);
 
     /**
-     * @param $oAuthRequestId
-     * @param $oAuthIdentifier
+     * @param string $oAuthRequestId
+     * @param string $oAuthIdentifier
      *
      * @return mixed
      */
-    public function loginOauth($oAuthRequestId, $oAuthIdentifier);
+    public function loginOauth(string $oAuthRequestId, string $oAuthIdentifier);
+
+    /**
+     *  Close the connection with FileMaker Server API
+     * @return mixed
+     * @throws Exception
+     */
+    public function logout();
 
     /**
      * @param       $layout
@@ -43,18 +50,45 @@ interface DataApiInterface
      * @param array $scripts
      *
      * @return mixed
+     * @throws Exception
      */
     public function editRecord($layout, $recordId, array $data, $lastModificationId = null, array $portalData = [], array $scripts = []);
 
     /**
+     * Duplicate an existing record
+     *
+     * @param       $layout
+     * @param       $recordId
+     * @param array $scripts
+     * @return mixed
+     * @throws Exception
+     */
+      public function duplicateRecord($layout, $recordId, array $scripts = []);
+
+      /**
+       * Delete record by id
+       *
+       * @param       $layout
+       * @param       $recordId
+       * @param array $scripts
+       *
+       * @throws Exception
+       */
+      public function deleteRecord($layout, $recordId, array $scripts = []);
+
+    /**
+     * Get record detail
+     *
      * @param       $layout
      * @param       $recordId
      * @param array $portalOptions
      * @param array $scripts
+     * @param null  $responseLayout
      *
      * @return mixed
+     * @throws Exception
      */
-    public function getRecord($layout, $recordId, array $portalOptions = [], array $scripts = []);
+    public function getRecord($layout, $recordId, array $portalOptions = [], array $scripts = [], $responseLayout = null);
 
     /**
      * @param       $layout
@@ -63,10 +97,11 @@ interface DataApiInterface
      * @param null  $limit
      * @param array $portals
      * @param array $scripts
+     * @param null  $responseLayout
      *
      * @return mixed
      */
-    public function getRecords($layout, $sort = null, $offset = null, $limit = null, array $portals = [], array $scripts = []);
+    public function getRecords($layout, $sort = null, $offset = null, $limit = null, array $portals = [], array $scripts = [], $responseLayout = null);
 
     /**
      * @param $layout
@@ -92,7 +127,18 @@ interface DataApiInterface
      *
      * @return mixed
      */
-    public function findRecords($layout, $query, $sort = null, $offset = null, $limit = null, array $portals = [], array $scripts = [], $responseLayout = null);
+    public function findRecords($layout, array $query, $sort = null, $offset = null, $limit = null, array $portals = [], array $scripts = [], $responseLayout = null);
+
+    /**
+     * Execute script alone
+     *
+     * @param     $layout
+     * @param     $scriptName
+     * @param null $scriptParam
+     * @return mixed
+     * @throws Exception
+     */
+    public function executeScript($layout, $scriptName, $scriptParam = null);
 
     /**
      * @param       $layout
@@ -103,21 +149,7 @@ interface DataApiInterface
     public function setGlobalFields($layout, array $globalFields);
 
     /**
-     * @param       $layout
-     * @param       $recordId
-     * @param array $scripts
-     *
      * @return mixed
-     */
-    public function deleteRecord($layout, $recordId, $scripts = []);
-
-    /**
-    * @return mixed
-    */
-    public function logout();
-
-    /**
-     * @return string|null
      */
     public function getApiToken();
 
@@ -126,7 +158,21 @@ interface DataApiInterface
      *
      * @return True|False
      */
-    public function setApiToken($token);
+    public function setApiToken(string $token, $date = null);
+
+    /**
+     * returns API token last use date, or False if there is no last use date.
+     *
+     * @return string|False
+     */
+    public function getApiTokenDate();
+
+    /**
+    * sets api token last used date. for internal library use. token lifetime is reset on each use in FM DAPI, hence this.
+    *
+    * @return True|False
+    */
+    public function setApiTokenDate();
 
     /**
      * @return True|False
@@ -134,7 +180,40 @@ interface DataApiInterface
     public function isApiTokenExpired();
 
     /**
-     * @return True|False
+     * @return True/False
      */
     public function refreshToken();
+
+    /**
+     * @return mixed
+     * @throws Exception
+     */
+    public function getProductInfo();
+
+    /**
+     * @return mixed
+     * @throws Exception
+     */
+    public function getDatabaseNames();
+
+    /**
+     * @return mixed
+     * @throws Exception
+     */
+    public function getLayoutNames();
+
+    /**
+     * @return mixed
+     * @throws Exception
+     */
+    public function getScriptNames();
+
+    /**
+     * @param $layout
+     * @param null $recordId
+     *
+     * @throws Exception
+     * @return mixed
+     */
+    public function getLayoutMetadata($layout, $recordId = null);
 }
